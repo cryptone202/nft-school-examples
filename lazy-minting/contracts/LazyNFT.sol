e82bf67be63510a725b558a6cce34251aaf974d4
cryptone202 <93718546+cryptone202@users.noreply.github.com>
2022-03-31 11:01:03 +0200

    Update LazyNFT.sol

--- lazy-minting/contracts/LazyNFT.sol
+++ lazy-minting/contracts/LazyNFT.sol
@@ -1,118 +1,84 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/StorageSlot.sol)
+
 pragma solidity ^0.8.0;
-pragma abicoder v2; // required to accept structs as function parameters
 
-import "hardhat/console.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
-
-contract LazyNFT is ERC721URIStorage, EIP712, AccessControl {
-  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-  string private constant SIGNING_DOMAIN = "LazyNFT-Voucher";
-  string private constant SIGNATURE_VERSION = "1";
-
-  mapping (address => uint256) pendingWithdrawals;
-
-  constructor(address payable minter)
-    ERC721("LazyNFT", "LAZ") 
-    EIP712(SIGNING_DOMAIN, SIGNATURE_VERSION) {
-      _setupRole(MINTER_ROLE, minter);
+/**
+ * @dev Library for reading and writing primitive types to specific storage slots.
+ *
+ * Storage slots are often used to avoid storage conflict when dealing with upgradeable contracts.
+ * This library helps with reading and writing to such slots without the need for inline assembly.
+ *
+ * The functions in this library return Slot structs that contain a `value` member that can be used to read or write.
+ *
+ * Example usage to set ERC1967 implementation slot:
+ * ```
+ * contract ERC1967 {
+ *     bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+ *
+ *     function _getImplementation() internal view returns (address) {
+ *         return StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value;
+ *     }
+ *
+ *     function _setImplementation(address newImplementation) internal {
+ *         require(Address.isContract(newImplementation), "ERC1967: new implementation is not a contract");
+ *         StorageSlot.getAddressSlot(_IMPLEMENTATION_SLOT).value = newImplementation;
+ *     }
+ * }
+ * ```
+ *
+ * _Available since v4.1 for `address`, `bool`, `bytes32`, and `uint256`._
+ */
+library StorageSlot {
+    struct AddressSlot {
+        address value;
     }
 
-  /// @notice Represents an un-minted NFT, which has not yet been recorded into the blockchain. A signed voucher can be redeemed for a real NFT using the redeem function.
-  struct NFTVoucher {
-    /// @notice The id of the token to be redeemed. Must be unique - if another token with this ID already exists, the redeem function will revert.
-    uint256 tokenId;
-
-    /// @notice The minimum price (in wei) that the NFT creator is willing to accept for the initial sale of this NFT.
-    uint256 minPrice;
-
-    /// @notice The metadata URI to associate with this token.
-    string uri;
-
-    /// @notice the EIP-712 signature of all other fields in the NFTVoucher struct. For a voucher to be valid, it must be signed by an account with the MINTER_ROLE.
-    bytes signature;
-  }
-
-
-  /// @notice Redeems an NFTVoucher for an actual NFT, creating it in the process.
-  /// @param redeemer The address of the account which will receive the NFT upon success.
-  /// @param voucher A signed NFTVoucher that describes the NFT to be redeemed.
-  function redeem(address redeemer, NFTVoucher calldata voucher) public payable returns (uint256) {
-    // make sure signature is valid and get the address of the signer
-    address signer = _verify(voucher);
-
-    // make sure that the signer is authorized to mint NFTs
-    require(hasRole(MINTER_ROLE, signer), "Signature invalid or unauthorized");
-
-    // make sure that the redeemer is paying enough to cover the buyer's cost
-    require(msg.value >= voucher.minPrice, "Insufficient funds to redeem");
-
-    // first assign the token to the signer, to establish provenance on-chain
-    _mint(signer, voucher.tokenId);
-    _setTokenURI(voucher.tokenId, voucher.uri);
-    
-    // transfer the token to the redeemer
-    _transfer(signer, redeemer, voucher.tokenId);
-
-    // record payment to signer's withdrawal balance
-    pendingWithdrawals[signer] += msg.value;
-
-    return voucher.tokenId;
-  }
-
-  /// @notice Transfers all pending withdrawal balance to the caller. Reverts if the caller is not an authorized minter.
-  function withdraw() public {
-    require(hasRole(MINTER_ROLE, msg.sender), "Only authorized minters can withdraw");
-    
-    // IMPORTANT: casting msg.sender to a payable address is only safe if ALL members of the minter role are payable addresses.
-    address payable receiver = payable(msg.sender);
-
-    uint amount = pendingWithdrawals[receiver];
-    // zero account before transfer to prevent re-entrancy attack
-    pendingWithdrawals[receiver] = 0;
-    receiver.transfer(amount);
-  }
-
-  /// @notice Retuns the amount of Ether available to the caller to withdraw.
-  function availableToWithdraw() public view returns (uint256) {
-    return pendingWithdrawals[msg.sender];
-  }
-
-  /// @notice Returns a hash of the given NFTVoucher, prepared using EIP712 typed data hashing rules.
-  /// @param voucher An NFTVoucher to hash.
-  function _hash(NFTVoucher calldata voucher) internal view returns (bytes32) {
-    return _hashTypedDataV4(keccak256(abi.encode(
-      keccak256("NFTVoucher(uint256 tokenId,uint256 minPrice,string uri)"),
-      voucher.tokenId,
-      voucher.minPrice,
-      keccak256(bytes(voucher.uri))
-    )));
-  }
-
-  /// @notice Returns the chain id of the current blockchain.
-  /// @dev This is used to workaround an issue with ganache returning different values from the on-chain chainid() function and
-  ///  the eth_chainId RPC method. See https://github.com/protocol/nft-website/issues/121 for context.
-  function getChainID() external view returns (uint256) {
-    uint256 id;
-    assembly {
-        id := chainid()
+    struct BooleanSlot {
+        bool value;
     }
-    return id;
-  }
 
-  /// @notice Verifies the signature for a given NFTVoucher, returning the address of the signer.
-  /// @dev Will revert if the signature is invalid. Does not verify that the signer is authorized to mint NFTs.
-  /// @param voucher An NFTVoucher describing an unminted NFT.
-  function _verify(NFTVoucher calldata voucher) internal view returns (address) {
-    bytes32 digest = _hash(voucher);
-    return ECDSA.recover(digest, voucher.signature);
-  }
+    struct Bytes32Slot {
+        bytes32 value;
+    }
 
-  function supportsInterface(bytes4 interfaceId) public view virtual override (AccessControl, ERC721) returns (bool) {
-    return ERC721.supportsInterface(interfaceId) || AccessControl.supportsInterface(interfaceId);
-  }
+    struct Uint256Slot {
+        uint256 value;
+    }
+
+    /**
+     * @dev Returns an `AddressSlot` with member `value` located at `slot`.
+     */
+    function getAddressSlot(bytes32 slot) internal pure returns (AddressSlot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+
+    /**
+     * @dev Returns an `BooleanSlot` with member `value` located at `slot`.
+     */
+    function getBooleanSlot(bytes32 slot) internal pure returns (BooleanSlot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+
+    /**
+     * @dev Returns an `Bytes32Slot` with member `value` located at `slot`.
+     */
+    function getBytes32Slot(bytes32 slot) internal pure returns (Bytes32Slot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+
+    /**
+     * @dev Returns an `Uint256Slot` with member `value` located at `slot`.
+     */
+    function getUint256Slot(bytes32 slot) internal pure returns (Uint256Slot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
 }
